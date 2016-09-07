0f16345bc86a9ff60789f0ef5241905e5b7e6014
Janny <187231153@qq.com>
2016-09-07 10:04:27 +0800

    Version4

--- CPSTest.py
+++ CPSTest.py
@@ -1,40 +1,19 @@
 import os
-import win32file
-import win32con
 import time
 import shutil
-from os import listdir
 
-ACTIONS = {
-    1: "Created",
-    2: "Deleted",
-    3: "Updated",
-    4: "Renamed from something",
-    5: "Renamed to something"
-}
+def copyfile():
+    date = time.strftime("%Y%m%d", time.localtime(time.time()))
+    print(date)
+    source_dir = "\\\\10.193.236.88\\dropfolder\\RM2.0CI_CDC\\RM2.0CI_CDC_" + date + ".1\\Install\\Suite\\PCR_NA"
+    target_dir = "D:\Target\CPS"
+    if os.path.exists(target_dir):
+        os.remove(target_dir)
+    shutil.copytree(source_dir, target_dir)
 
-FILE_LIST_DIRECTORY = win32con.GENERIC_READ
-path_to_watch=r"\\zch49app08\CG546\8.Temp\Janny"
-hDir = win32file.CreateFile(
-    path_to_watch,
-    FILE_LIST_DIRECTORY,
-    win32con.FILE_SHARE_READ,
-    None,
-    win32con.OPEN_EXISTING,
-    win32con.FILE_FLAG_BACKUP_SEMANTICS,
-    None
-)
 
 if __name__ == '__main__':
-    time=time.strftime("%Y%m%d",time.localtime(time.time()))
-    print(time)
-    sourceDir="\\\\10.193.236.88\\dropfolder\\RM2.0CI_CDC\\RM2.0CI_CDC_"+time+".1\\Install\\Suite\\PCR_NA"
-    targetDir="D:\Target\CPS"
-    copyFileCounts=0
-    if os.path.exists(targetDir):
-        os.remove(targetDir)
-    shutil.copytree(sourceDir, targetDir)
-    print("End")
+    copyfile()
 
 
 
